60b799713c20f10bcbdbd20c603d6c8db272441a
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-01-10 11:29:24 +0000

    part1 progress

--- day23/part1.py
+++ day23/part1.py
@@ -24,88 +24,120 @@ class Amber:
   def __init__(self, row, col):
     self.row = row
     self.col = col
+    # Amphipods are immobile once moved unless can reach destination
     self.moved = False
     self.energy = 1
+    # The destinations for this type of amphipod
+    self.secondary_spot = (2,3)
+    self.primary_spot = (3,3)
 class Bronze:
   def __init__(self, row, col):
     self.row = row
     self.col = col
     self.moved = False
     self.energy = 10
+    self.secondary_spot = (2,5)
+    self.primary_spot = (3,5)
 class Copper:
   def __init__(self, row, col):
     self.row = row
     self.col = col
     self.moved = False
     self.energy = 100
+    self.secondary_spot = (2,7)
+    self.primary_spot = (3,7)
 class Desert:
   def __init__(self, row, col):
     self.row = row
     self.col = col
     self.moved = False
     self.energy = 1000
+    self.secondary_spot = (2,9)
+    self.primary_spot = (3,9)
 
-# forbidden moves are global
-forbidden_moves = [(1, 3), (1, 5), (1, 7)]
+# global rule: cannot stop outside of any room 
+forbidden_moves = [(1, 3), (1, 5), (1, 7), (1, 9)]
 
-def dfs_move(row, col, a_map, start, seen=[]):
-
+def dfs_move(amphipod, row, col, a_map, seen, start=False):
+  # Prevent infinite DFS by returning when visiting a spot already evaluated
   if (row, col) in seen:
     return []
-
+  # add spot to seen list
   seen.append((row, col))
-
+  # We don't want to evaluate the start position, return when we reach a non-empty space
   if start == False and a_map[row][col] != '.':
     return []
 
   moves = []
 
-  if start == False and a_map[row][col] == '.':
+  # if this spot is an empty space we can move to
+  if a_map[row][col] == '.':
+    # must not stop outside of any room
     if (row, col) not in forbidden_moves:
-      moves.append((row, col))
+      # destructure primary_spot tuple into row col indices
+      a,b = amphipod.primary_spot
+      # always allow move to primary_spot
+      if (row, col) == amphipod.primary_spot:
+        moves.append((row, col))
+      # always allow move to secondary_spot if primary spot is correctly filled
+      elif (row, col) == amphipod.secondary_spot and type(amphipod) == type(a_map[a][b]):
+        moves.append((row, col))
+      # Only allow other moves if this amphipod is free to move
+      elif amphipod.moved == False:
+        moves.append((row, col))
 
   # down up right left
-  for move in dfs_move(row + 1, col, a_map, False, seen):
+  for move in dfs_move(amphipod, row + 1, col, a_map, seen):
     moves.append(move)
-  for move in dfs_move(row - 1, col, a_map, False, seen):
+  for move in dfs_move(amphipod, row - 1, col, a_map, seen):
     moves.append(move)
-  for move in dfs_move(row, col + 1, a_map, False, seen):
+  for move in dfs_move(amphipod, row, col + 1, a_map, seen):
     moves.append(move)
-  for move in dfs_move(row, col - 1, a_map, False, seen):
+  for move in dfs_move(amphipod, row, col - 1, a_map, seen):
     moves.append(move)
 
   return moves
 
-puzzle_input  = [line.replace("\n", "") for line in open('test.txt').readlines()]
-print(dfs_move(2, 3, puzzle_input, True))
+def get_energy_cost(start, end, energy_cost_per_move):
+  x_dist = abs(end[0] - start[0])
+  y_dist = abs(end[1] - start[1])
+  return energy_cost_per_move * (x_dist + y_dist)
 
-# def get_all_valid_moves(amphipods, a_map):
-#   moves = []
+assert(get_energy_cost((1,10), (3,3), 100)) == 900
 
-#   for amphipod in amphipods:
-#     dfs_move(amphipod.row, amphipod.col, True)
+def get_all_valid_moves(amphipods, a_map):
+  moves = []
+  for amphipod in amphipods:
+    # This amphipod's start pos
+    start = (amphipod.row, amphipod.col)
+    # Find valid moves for this amphipod
+    # For each move that exists: include start, end positions and energy cost
+    for move in dfs_move(amphipod, amphipod.row, amphipod.col, True):
+      # (start, dest, cost)
+      moves.append(start, move, get_energy_cost(start, move, amphipod.energy))
+  return moves
 
-# def main():
-#   puzzle_input  = [line.replace("\n", "") for line in open('test.txt').readlines()]
-#   # print(puzzle_input)
+def main():
+  puzzle_input  = [line.replace("\n", "") for line in open('test.txt').readlines()]
+  # print(puzzle_input)
 
-#   amphipods = []
+  amphipods = []
 
-#   for row in range(len(puzzle_input)):
-#     for col in range(len(puzzle_input[row])):
-#       match puzzle_input[row][col]:
-#         case 'A':
-#           amphipods.append(Amber(row, col))
-#         case 'B':
-#           amphipods.append(Bronze(row, col))
-#         case 'C':
-#           amphipods.append(Copper(row, col))
-#         case 'D':
-#           amphipods.append(Desert(row, col))
+  for row in range(len(puzzle_input)):
+    for col in range(len(puzzle_input[row])):
+      match puzzle_input[row][col]:
+        case 'A':
+          amphipods.append(Amber(row, col))
+        case 'B':
+          amphipods.append(Bronze(row, col))
+        case 'C':
+          amphipods.append(Copper(row, col))
+        case 'D':
+          amphipods.append(Desert(row, col))
 
-#   # print(amphipods)
-#   # for amphipod in amphipods:
-#   #   print(amphipod.row, amphipod.col, type(amphipod).__name__)
+  # print(amphipods)
+  # for amphipod in amphipods:
+  #   print(amphipod.row, amphipod.col, type(amphipod).__name__)
 
-# if __name__ == '__main__':
-#   main()
+if __name__ == '__main__':
+  main()
