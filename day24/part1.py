220db46bf95dc0a4d174104f4994e8110951c731
James Sue-Ling <james_sueling@hotmail.co.uk>
2022-01-13 10:35:28 +0000

    found long solution for part1

--- day24/part1.py
+++ day24/part1.py
@@ -51,20 +51,37 @@ def ALU(ALU_input,command_list):
 
   return 'valid' if z == 0 else 'invalid'
 
+def contains_zero_digit(integer): # https://stackoverflow.com/a/3847776
+  divisor = 10
+  remainder = 0
+
+  while divisor <= integer:
+    new_remainder = integer % divisor
+
+    if new_remainder - remainder == 0:
+      return True
+
+    divisor *= 10
+    remainder = new_remainder
+  return False
+
 def main():
   puzzle_input = [line.split() for line in open("input.txt")]
 
   # check all combinations of 14 digit 0-9
   # from largest 99999999999999 containing no 0 digits
 
-  ALU_input = '99999999999999'
-  print(ALU('13579246899999', puzzle_input))
+  ALU_input = 99999999999999
+
   while True:
-    # https://stackoverflow.com/a/51092473
-    result = ALU(ALU_input, puzzle_input)
-    if result == 'valid':
-      print("part1", ALU_input)
-      return
+    if not contains_zero_digit(ALU_input):
+      # https://stackoverflow.com/a/51092473
+      result = ALU(list(map(int, str(ALU_input))), puzzle_input)
+      print(ALU_input, result)
+      if result == 'valid':
+        print("part1", ALU_input)
+        return
+    ALU_input -= 1
 
 if __name__ == '__main__':
   main()
